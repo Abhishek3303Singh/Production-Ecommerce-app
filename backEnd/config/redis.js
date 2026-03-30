2920a164be6d36343a7c35458d0252f3e207d09c
Abhishek3303Singh <abhishekrajsingh136@gmail.com>
2026-03-30 23:47:09 +0530

    fix  cloud Redis url connection

--- backEnd/config/redis.js
+++ backEnd/config/redis.js
@@ -1,5 +1,10 @@
 const {createClient} = require( 'redis')
-const redisClient = createClient()
+console.log('url',process.env.REDIS_URL)
+const redisClient = createClient(
+    {
+        url: process.env.REDIS_URL
+    }
+)
 redisClient.on('error', (err) => console.log('Redis Error', err));
 (
     async ()=>{
